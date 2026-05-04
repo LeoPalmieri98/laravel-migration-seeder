6f0c2519d46eab879028d77889c30fcde1a7a8ce
leopii98 <rippergames98@gmail.com>
2026-05-04 18:56:29 +0200

    first commit

---
+++ database/migrations/2026_05_04_164954_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 100);
+            $table->string('stazione_partenza', 150);
+            $table->string('stazione_arrivo', 150);
+            $table->dateTime('orario_partenza');
+            $table->dateTime('orario_arrivo');
+            $table->string('codice_treno', 20);
+            $table->tinyInteger('totale_carrozze')->unsigned();
+            $table->boolean('in_orario')->default(true);
+            $table->boolean('cancellato')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
